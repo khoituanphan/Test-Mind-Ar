97a5d3f30a7c2fe26bf58ba9c1815fc27e790bad
khoituanphan <tuankhoi.phan0820@gmail.com>
2023-09-16 22:24:24 +0700

    update

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-import {loadGLTF} from "../../libs/loader.js";
+import {loadGLTF} from "./libs/loader.js";
 const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener('DOMContentLoaded', () => {
   const start = async() => {
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
-      imageTargetSrc: '../../assets/targets/robot.mind',
+      imageTargetSrc: './assets/targets/robot.mind',
     });
     const {renderer, scene, camera} = mindarThree;
 
     const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
     scene.add(light);
 
-    const robot = await loadGLTF('../../assets/models/robot/RobotExpressive.glb');
+    const robot = await loadGLTF('./assets/models/robot/RobotExpressive.glb');
     robot.scene.scale.set(0.2, 0.2, 0.2);
     robot.scene.position.set(0, -0.2, 0);
 
